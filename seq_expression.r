b348e542734e765805d81d00eb850d5437767e5f
rory-mcauley <rmcauley486@gmail.com>
2024-06-18 21:43:06 +0100

    Update seq_expression.r

--- seq_expression.r
+++ seq_expression.r
@@ -1,6 +1,9 @@
-for(i in 1:10){
-    print(i)
-}
+library(limma)
+library(edgeR)
 
+# Reading in the feature count file as "counts.df"
+counts.df <- read.csv("featCounts_S_cere_20200331.csv")
 
+# Printing the start of the counts.df object in R...
+head(counts.df)
 
