aa7314fc9ca07a5bdc90f9124fb5376231b17e57
rory-mcauley <rmcauley486@gmail.com>
2024-06-19 09:20:28 +0100

    Update seq_expression.r

--- seq_expression.r
+++ seq_expression.r
@@ -7,3 +7,11 @@ counts.df <- read.csv("featCounts_S_cere_20200331.csv")
 # Printing the start of the counts.df object in R...
 head(counts.df)
 
+# Using the "Geneid" column to set the rownames
+rownames(counts.df) <- counts.df$Geneid
+
+# Removing the "Geneid" column
+counts.df$Geneid <- NULL
+
+# Printing the start of the counts.df object in R...
+head(counts.df)
